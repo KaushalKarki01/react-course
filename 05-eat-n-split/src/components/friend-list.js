a664b21b21aaa887b66f98b1d09f89fa7c7fc068
kaushal <mail.kaushalkarki@gmail.com>
2024-11-25 12:01:48 +0545

    components arranged in separate folder

---
+++ 05-eat-n-split/src/components/friend-list.js
@@ -0,0 +1,15 @@
+import Friend from "./friend";
+export default function FriendsList({ friends, onSelection, selectedFriend }) {
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <Friend
+          friend={friend}
+          onSelection={onSelection}
+          selectedFriend={selectedFriend}
+          key={friend.id}
+        />
+      ))}
+    </ul>
+  );
+}

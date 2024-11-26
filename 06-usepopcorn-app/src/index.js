4138d5a6b00709389b9010e5e8aa0c9d5b897fa7
kaushal <mail.kaushalkarki@gmail.com>
2024-11-26 22:10:24 +0545

    worked on fetching data from api and setting the loader

--- 06-usepopcorn-app/src/index.js
+++ 06-usepopcorn-app/src/index.js
@@ -1,26 +1,27 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
-import StarRating from "./StarRating";
 // import App from "./App";
-// import "./index.css";
+import Appv1 from "./Appv1";
+import "./index.css";
 
-function Test() {
-  const [MovieRating, setMovieRating] = useState(0);
-  return (
-    <div>
-      <StarRating onSetRating={setMovieRating} maxRating={8} />
-      <p>The rating is {MovieRating}</p>
-    </div>
-  );
-}
+// function Test() {
+//   const [MovieRating, setMovieRating] = useState(0);
+//   return (
+//     <div>
+//       <StarRating onSetRating={setMovieRating} maxRating={8} />
+//       <p>The rating is {MovieRating}</p>
+//     </div>
+//   );
+// }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={4} />
+    {/* <StarRating maxRating={4} />
     <StarRating size={24} color="red" maxRating={10} />
     <StarRating size={24} color="green" defaultRating={3} maxRating={7} />
-    <Test />
+    <Test /> */}
+    <Appv1 />
   </React.StrictMode>
 );

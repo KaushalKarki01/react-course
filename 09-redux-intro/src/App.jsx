23e519b11d73db6a3eef37e63945ccca7127f413
kaushal <mail.kaushalkarki@gmail.com>
2024-12-09 21:51:18 +0545

    react-redux intro code using thunks

---
+++ 09-redux-intro/src/App.jsx
@@ -0,0 +1,25 @@
+import { useSelector } from "react-redux";
+import AccountOperations from "./features/account/AccountOperations";
+import BalanceDisplay from "./features/account/BalanceDisplay";
+import { default as CreateCustomer } from "./features/customer/CreateCustomer";
+import Customer from "./features/customer/Customer";
+
+function App() {
+  const fullName = useSelector((state) => state.customer.fullName);
+  return (
+    <div>
+      <h1>🏦 The React-Redux Bank ⚛️</h1>
+      {fullName === "" ? (
+        <CreateCustomer />
+      ) : (
+        <>
+          <Customer />
+          <AccountOperations />
+          <BalanceDisplay />
+        </>
+      )}
+    </div>
+  );
+}
+
+export default App;
